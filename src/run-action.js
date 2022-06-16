71f6c989b4dda1ad0295c85f92655b897d1e5467
Rufat Khaslarov <rufat.khaslarov@orca.security>
2022-06-16 17:53:41 +0300

    change label config

--- src/run-action.js
+++ src/run-action.js
@@ -6,29 +6,29 @@ const existsLabel = require('./github/exists-label');
 const getLabelConfig = (tools) => {
   const labelConfig = [
     {
-      name: 'size_xs',
+      name: 'XS',
       size: tools.inputs.xs_max_size,
-      color: 'abdee6',
+      color: '97efb7',
     },
     {
-      name: 'size_s',
+      name: 'S',
       size: tools.inputs.s_max_size,
-      color: 'cbaacb',
+      color: '7ad39c',
     },
     {
-      name: 'size_m',
+      name: 'M',
       size: tools.inputs.m_max_size,
-      color: 'ffaea5',
-    },
-    {
-      name: 'size_l',
-      size: tools.inputs.l_max_size,
       color: 'ffffb5',
     },
     {
-      name: 'size_xl',
+      name: 'L',
+      size: tools.inputs.l_max_size,
+      color: 'ffaea5',
+    },
+    {
+      name: 'XL',
       size: Infinity,
-      color: 'cce2cb',
+      color: 'ff7a7a',
     },
   ];
   return labelConfig;
@@ -49,20 +49,28 @@ const createLabelsIfNotExists = async (tools, labelConfig) => {
 const getNumberOfLines = async (tools) => {
   try {
     tools.log.info(`Getting the number of lines`);
+
     const { data: files } = await tools.github.pulls.listFiles({
       ...tools.context.repo,
       pull_number: tools.context.issue.number,
     });
+
     const numberOfLines = files.reduce((accumulator, file) => {
+
       if (file.filename.match(tools.inputs.exclude_files)) {
         tools.log.info(`Excluding file from the counting ${file.filename}`);
         return accumulator;
       }
+      
       tools.log.info(`Adding file to the counting: ${file.filename} The number of lines is: ${file.changes}`);
+
       return accumulator + file.changes;
     }, 0);
+
     tools.log.info(`Number of lines changed: ${numberOfLines}`);
+
     return numberOfLines;
+
   } catch (error) {
     tools.log.info(
       `Error happens when we listing the files of the pull request: ${error}`,
@@ -81,6 +89,7 @@ const assignLabelForLineChanges = async (tools, numberOfLines, labelConfig) => {
   );
 
   const element = labelConfig.find((elem) => numberOfLines <= elem.size);
+
   if (element) {
     await addLabel(tools, element.name);
   }
